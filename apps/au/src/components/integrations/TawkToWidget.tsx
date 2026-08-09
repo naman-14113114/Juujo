bd45e6f48c88138eeacd3ddeb7336acc072ca430
Sahil Jain <sahiljainsj004@gmail.com>
2026-08-09 19:23:15 +0530

    feat: add TawkToWidget integration component with lazy-loading and dynamic positioning to all regional apps

--- apps/au/src/components/integrations/TawkToWidget.tsx
+++ apps/au/src/components/integrations/TawkToWidget.tsx
@@ -11,7 +11,7 @@ Tawk_API.customStyle = {
 (function(){
 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
 s1.async=true;
-s1.src='https://embed.tawk.to/';
+s1.src='https://embed.tawk.to/67be08f3dfac1e1910d845f9/1ikv55u3b';
 s1.charset='UTF-8';
 s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);
